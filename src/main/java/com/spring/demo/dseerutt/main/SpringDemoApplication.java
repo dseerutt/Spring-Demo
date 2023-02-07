c4353125fb5cc4a58a4fc7dbd1b17fa3917d4da0
Dan <dan.seeruttun@gmail.com>
2023-02-07 21:20:44 +0100

    Set log4j as program logger

--- src/main/java/com/spring/demo/dseerutt/main/SpringDemoApplication.java
+++ src/main/java/com/spring/demo/dseerutt/main/SpringDemoApplication.java
@@ -1,5 +1,7 @@
 package com.spring.demo.dseerutt.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,20 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class SpringdemoApplication implements ApplicationRunner {
+public class SpringDemoApplication implements ApplicationRunner {
     private final String HEALTH_CHECK = "/health";
 
+    private static final Logger LOGGER = LogManager.getLogger(SpringDemoApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringdemoApplication.class, args);
+        SpringApplication.run(SpringDemoApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments arg0) throws Exception {
-        System.out.println("Starting the webservices");
+        LOGGER.info("Starting the webservices");
     }
 
     @RequestMapping(value = HEALTH_CHECK)
     public String getHealthCheck() {
+        LOGGER.info("Health Check webservice is called");
         return "I am up and running";
     }
 
